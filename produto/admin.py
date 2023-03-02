97802e311820174631b1ffe8c5e8e537423f5e7a
Anderson <derson_gm@hotmail.com>
2023-03-02 15:12:04 -0300

    models(variação, produto)

--- produto/admin.py
+++ produto/admin.py
@@ -2,4 +2,23 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.Produto)
+
+class VariacaoInline(admin.TabularInline):
+    """
+    exibir um campo extra em branco para facilitar a criação de uma nova 
+    variação
+    """
+    model = models.Variacao
+    extra = 1
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    # quando entrar no produto, define quais os 'filhos' desse produto serão
+    # exibidos
+    inlines = [
+        VariacaoInline
+    ]
+
+
+admin.site.register(models.Produto, ProdutoAdmin)
+admin.site.register(models.Variacao)
--- produto/models.py
+++ produto/models.py
@@ -1,12 +1,19 @@
+from pathlib import Path
+
+from django.conf import settings
 from django.db import models
+from PIL import Image
 
 
 class Produto(models.Model):
+    """
+    Classe para criação de um produto com imagem, descrição, e preço
+    """
     nome = models.CharField(max_length=255)
     descricao_curta = models.TextField(max_length=255)
     descricao_longa = models.TextField()
     imagem = models.ImageField(
-        upload_to='produto_omagens/%Y/%m/', blank=True, null=True)
+        upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
     slug = models.SlugField(unique=True)
     preco_marketing = models.FloatField()
     preco_marketing_promocional = models.FloatField(default=0)
@@ -19,5 +26,52 @@ class Produto(models.Model):
         )
     )
 
+    @staticmethod
+    def resize_image(img, new_width=800):
+        # função para redimensionar a imagem do produto a 800pxls
+        img_full_path = Path.joinpath(settings.MEDIA_ROOT, img.name)
+        img_pil = Image.open(img_full_path)
+        original_width, original_height = img_pil.size
+
+        if original_width <= new_width:
+            img_pil.close()
+            return
+
+        new_height = round((new_width * original_height) / original_width)
+        new_img = img_pil.resize((new_width, new_height), Image.LANCZOS)
+        new_img.save(
+            img_full_path,
+            optimize=True,
+            quality=50
+        )
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        max_image_size = 800
+
+        if self.imagem:
+            self.resize_image(self.imagem, max_image_size)
+
     def __str__(self):
         return self.nome
+
+
+class Variacao(models.Model):
+    """
+    classe para criação de variações do produto principal
+    """
+    # se o produto for deletado, todas as variações tmb serão
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveBigIntegerField(default=1)
+
+    def __str__(self):
+        return self.nome or self.produto.nome
+
+    class Meta:
+        # Arrumar o plural das variações
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
