cf3ff8384df1333708cd7245d30d824eec283129
Anderson <derson_gm@hotmail.com>
2023-03-06 15:26:54 -0300

    models(pedido, itempedido)

--- pedido/models.py
+++ pedido/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Pedido(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total = models.FloatField()
+    status = models.CharField(
+        default='C',
+        max_length=1,
+        choices=(
+            ('A', 'Aprovado'),
+            ('C', 'Criado'),
+            ('R', 'Reprovado'),
+            ('P', 'Pendente'),
+            ('E', 'Enviado'),
+            ('F', 'Finalizado'),
+        )
+    )
+
+    def __str__(self):
+        return f'Pedido N° {self.pk}'
+
+
+class ItemPedido(models.Model):
+    order = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    product = models.CharField(max_length=255)
+    product_id = models.PositiveBigIntegerField()
+    varation = models.CharField(max_length=255)
+    variation_id = models.PositiveBigIntegerField()
+    price = models.FloatField()
+    promocional_price = models.FloatField(default=0)
+    quantity = models.PositiveBigIntegerField()
+    image = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return f'Item do {self.order}'
+
+    class Meta:
+        verbose_name = 'Item do pedido'
+        verbose_name_plural = 'Itens do pedido'
